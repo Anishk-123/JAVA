c0b1244ea652d4ca6db1b3decb7df6130450dc87
Anishk-123 <anish.23cs014@sode-edu.in>
2024-11-12 15:45:35 +0530

    About nested Class

---
+++ OuterClass.java
@@ -0,0 +1,27 @@
+package k3;
+
+public class OuterClass {
+	
+	   
+	    private String message = "Hello from the Outer Class!";
+
+	    
+	    class InnerClass {
+	        
+	        void display() {
+	            System.out.println(message);
+	        }
+	    }
+
+	    public static void main(String[] args) {
+	        
+	        OuterClass outer = new OuterClass();
+	        
+	        
+	        OuterClass.InnerClass inner = outer.new InnerClass();
+	        
+	        
+	        inner.display();
+	    }
+	}
+
