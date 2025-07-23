fdcd2a82bfde12fa3adb9a37885d37473c0ed021
Anishk-123 <anish.23cs014@sode-edu.in>
2025-07-23 09:40:01 +0530

    About Generic Method

---
+++ Demo.java
@@ -0,0 +1,8 @@
+public class Demo {
+
+    <T> void genricprint(T t) {
+        System.out.println(t);
+
+    }
+
+}
--- Main.java
+++ Main.java
@@ -1,32 +1,10 @@
-package k3;
-
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-	
-	class Animal {
-
-	    void sound() {
-	        System.out.println("Animal makes a sound");
-	    }
-	}
-
-	
-	class Dog extends Animal {
-	   
-	    void sound() {
-	        System.out.println("Dog barks");
-	    }
-	}
-
-	public class Main {
-	    public static void main(String[] args) {
-	        // Creating an object of the child class
-	        Dog myDog = new Dog();
-	        myDog.sound(); // Calls the overridden method in Dog class
-
-	        // Creating an object of the parent class
-	        Animal myAnimal = new Animal();
-	        myAnimal.sound(); // Calls the method in Animal class
-	    }
-	
-	}
+    public static void main(String[] args) {
+        Demo b=new Demo();
+        b.genricprint(101);
+        b.genricprint("joy");
+        b.genricprint("3.14");
+    }
 }
