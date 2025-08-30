e9d6339138181d7887f3e19adfe2fa8f3a9eed00
Anishk-123 <anish.23cs014@sode-edu.in>
2025-08-30 22:39:19 +0530

    Finding the factorial of a given number.

--- Factorial.java
+++ Factorial.java
@@ -1,21 +1,29 @@
-package k3;
+package Pratice;
+
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
 
 public class Factorial {
-	public static int factorial(int n) {
-		if(n==0) {
-			return 1;
-		}
-		else {
-			return n*factorial(n-1);
-		}
-		
-	}
-	public static void main(String[]args) {
-		int number=5;
-		int result=factorial(number);
-		System.out.println("factorial="+result);
-		}
-	}
+    public static void main(String[] args) {
 
+        System.out.println("Enter a value");
+        Scanner sc1=new Scanner(System.in);
+        int n=sc1.nextInt();
+        if(n<0){
+            System.out.println("factorial of negative number is not possibe");
+        }
+        if(n == 1 || n == 0){
+            System.out.println("Factorial of value = 1");
+        }
+        else{
+            int fact=1;
+            for(int i=2;i<=n;i++){
+                fact=fact*i;
 
+            }
+            System.out.println(fact);
+        }
 
+    }
+}
