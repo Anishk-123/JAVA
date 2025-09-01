fd2c885a2e1a7f817090ab70529d2f53a89df594
Anishk-123 <anish.23cs014@sode-edu.in>
2025-09-01 21:17:28 +0530

    Add files via upload

---
+++ TwoSum.java
@@ -0,0 +1,13 @@
+package Pratice;
+
+public class TwoSum {
+    int arr[]={1,2,3,4,8};
+    int n=arr.length;
+    int sum=0;
+    for(int i=0;i<n;i++){
+        sum=sum+arr[i];
+        if()
+
+
+    }
+}
